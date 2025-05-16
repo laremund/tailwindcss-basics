2dd8aa9992d432919e2cc6d9234679dd560c9de3
laremund <larsgremund@gmail.com>
2025-05-16 13:49:49 -0600

    Initial commit, deleted some of the default vite stuff so that I can make a little app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1 className='text-red-400'>Vite + React</h1>
+      <div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
