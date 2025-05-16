3c5902d56d79380a1130eb0c2f0da2e7ea64441f
laremund <larsgremund@gmail.com>
2025-05-16 13:52:10 -0600

    feat(App): added tags for app, no styling yet

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,40 @@ function App() {
 
   return (
     <>
+     <div>
       <div>
-
+        <h1>Dad Joke Generator</h1>
+        <p>Prepare to groan!</p>
+        
+        <div>
+          <div>
+            <p>I told my wife she was drawing her eyebrows too high. She looked surprised.</p>
+          </div>
+          
+          <div>
+            <span>Rating: 0</span>
+          </div>
+          
+          <div>
+            <button>
+              Groan
+            </button>
+            
+            <button>
+              Next Joke
+            </button>
+            
+            <button>
+              Chuckle
+            </button>
+          </div>
+        </div>
+        
+        <div>
+          <p>You've seen 0 of 10 available jokes</p>
+        </div>
       </div>
-      <h1 className='text-red-400'>Vite + React</h1>
-      <div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
